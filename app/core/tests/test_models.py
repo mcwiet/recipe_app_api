376e46afcc34f17a27d4a47c7579a8978bdbaa64
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-10-10 18:40:38 -0400

    Adding email normalization, email validation, and ability to create superuser. Also fixing some flake8 errors.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -14,3 +15,25 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_normalize_user_email(self):
+        """Test that email gets normalized to lowercase when creating user"""
+        email = 'test@MIKE.COM'
+        user = get_user_model().objects.create_user(email,'1password')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_valid_email_address(self):
+        """Test that an creating a user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '1pass')
+
+    def test_create_super_user(self):
+        """Test create super user that is staff and super user"""
+        user = get_user_model().objects.create_superuser(
+            'email@email.com',
+            '1password'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
